b50005724477f3722ad0fb17661e1ba952279909
Gazal <gtech5277@gmail.com>
2025-03-15 18:45:59 +0530

    preprocessing done

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Tokenize 
+<!-- To Instantiate tokenize Class : -->
+```
+tokenizer = TweetTokenizer(preserve_case = False, strip_handles = True, reduce_len = True)
+```
+<!-- and then tokinze using : output = tokenizer.tokenize(input) -->
+
+# Remove Stop words and punctuations           
+<!-- import english stop words list. -->
+
+```
+stopwords_english = stopwords.words('english')
+```
+<!-- To create a clean list : -->
+```
+for word in tweet_tokens:
+    if (word not in stopwords_english and word not in string.punctuation):
+        tweet_clean.append(word)
+```
+
--- main.py
+++ main.py
@@ -12,36 +12,76 @@ from nltk.tokenize import TweetTokenizer
 nltk.download('twitter_samples')
 nltk.download('stopwords')
 
-# pos and neg tweets
-pos_tweets = twitter_samples.strings('positive_tweets.json')
-neg_tweets = twitter_samples.strings('negative_tweets.json')
-print(f'Pos Tweets : {pos_tweets} | type : {type(pos_tweets)}')
-print(f'Neg Tweets : {neg_tweets} | type : {type(neg_tweets)}')
+def biclass_plot(class_a, class_b):
 
-# plotting information 
+    fig = plt.figure(figsize = (5,5))
+    labels = 'Positive', 'Negative'
+    size = [len(pos_tweets), len(neg_tweets)]
+    plt.pie(size,labels = labels, autopct = '%1.1f%%', shadow = True, startangle = 70)
+    plt.axis('equal')
+    plt.show()
 
-# figure
-fig = plt.figure(figsize = (5,5))
-# labels
-labels = 'Positive', 'Negative'
-# size
-size = [len(pos_tweets), len(neg_tweets)]
-# plot 
-plt.pie(size,labels = labels, autopct = '%1.1f%%', shadow = True, startangle = 70)
-# equal aspect ratio
-plt.axis('equal')
-# display
-plt.show()
+def classify(dataset):
+    pos_tweets = dataset.strings('positive_tweets.json')
+    neg_tweets = dataset.strings('negative_tweets.json')
 
-# looking at raq texts
-# print('\033[92m' +  pos_tweets[random.randint(0,5000)])
-# print('\033[91m' +  neg_tweets[random.randint(0,5000)])
+    # biclass_plot(pos_tweets, neg_tweets)
 
-# preprocessing raw texts for sentiment analysis
+    return pos_tweets,neg_tweets
 
-# choose a sample pos tweet
-tweet = pos_tweets[random.randint(0,5000)]
-# remove hyperlinks
-tweet2 = re.sub(r'https?://[^\s\n\r]+', '', tweet)
-# remove hashtags
-tweet2 = re.sub(r'#','', tweet2)
+def preprocess(class_i):
+    tweet = class_i[random.randint(0,len(class_i))]
+    # remove hyperlinks
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    # remove hashtags
+    tweet = re.sub(r'#','', tweet)
+
+    return tweet
+
+
+def tokenize_string(string_i):
+    tokenizer = TweetTokenizer(preserve_case = False, strip_handles = True, reduce_len = True)
+    return tokenizer.tokenize(string_i)
+
+def remove_stopwords(list_i):
+    stopwords_english = stopwords.words('english')
+    clean = []
+    for word in list_i:
+        if ( word not in stopwords_english and word not in string.punctuation):
+            clean.append(word)
+    return clean
+
+def stemming(list_i):
+    stemmer = PorterStemmer()
+    stem = []
+    for word in list_i:
+        stem_word = stemmer.stem(word)
+        stem.append(stem_word)
+
+    return stem
+
+# Classify
+pos_tweets,neg_tweets = classify(twitter_samples)
+
+# Preprocess
+tweet = preprocess(pos_tweets)
+
+
+# Tokenize
+tweet_tokens = tokenize_string(tweet)
+
+# Remove Stopwords
+tweet_clean = remove_stopwords(tweet_tokens)
+
+# Stemming
+tweets_stem = stemming(tweet_clean)
+
+# Instead of all this manual processing we can use
+# process_tweet
+
+from utils import process_tweet
+
+stem_tweets = process_tweet(tweet)
+
+# Result Match
+print(stem_tweets, "   ", tweets_stem)
